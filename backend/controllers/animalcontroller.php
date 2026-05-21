82bce16c0fcd0ba2ee86c0e24ddf9555f75f8647
MCristi028 <manolachecristian06@gmail.com>
2026-05-21 22:52:45 +0300

    Added animal filtering API

--- backend/controllers/animalcontroller.php
+++ backend/controllers/animalcontroller.php
@@ -5,20 +5,38 @@ require_once __DIR__ . '/../views/JsonView.php';
 
 class AnimalController
 {
-    private $animalModel;
+    private $model;
 
-    public function __construct($connection)
+    public function __construct($dbConnection)
     {
-        $this->animalModel = new AnimalModel($connection);
+        $this->model = new AnimalModel($dbConnection);
     }
 
     public function getAnimals()
     {
-        $animals = $this->animalModel->getAllAnimals();
+        $filters = [
+            'id_clasa' => isset($_GET['id_clasa']) ? (int) $_GET['id_clasa'] : null,
+            'id_origine' => isset($_GET['id_origine']) ? (int) $_GET['id_origine'] : null,
+            'id_regim' => isset($_GET['id_regim']) ? (int) $_GET['id_regim'] : null,
+            'id_clima' => isset($_GET['id_clima']) ? (int) $_GET['id_clima'] : null,
+            'are_blana' => isset($_GET['are_blana']) ? (int) $_GET['are_blana'] : null,
+            'poate_fi_dresat' => isset($_GET['poate_fi_dresat']) ? (int) $_GET['poate_fi_dresat'] : null,
+            'este_periculos' => isset($_GET['este_periculos']) ? (int) $_GET['este_periculos'] : null
+        ];
+
+        $activeFilters = array_filter($filters, function ($value) {
+            return $value !== null;
+        });
+
+        $animals = $this->model->getFilteredAnimals($activeFilters);
 
         JsonView::render([
             "status" => "success",
-            "data" => $animals
+            "rezultate" => count($animals),
+            "data" => $animals,
+            "message" => count($animals) === 0
+                ? "Nu s-au gasit animale care sa corespunda acestor criterii."
+                : null
         ]);
     }
 }
--- backend/models/animalmodel.php
+++ backend/models/animalmodel.php
@@ -4,30 +4,41 @@ class AnimalModel
 {
     private $conn;
 
-    public function __construct($connection)
+    public function __construct($dbConnection)
     {
-        $this->conn = $connection;
+        $this->conn = $dbConnection;
     }
 
-    public function getAllAnimals()
+    public function getFilteredAnimals($filters)
     {
         $sql = "
             SELECT
                 a.id_animal,
                 a.nume_popular,
                 a.nume_stiintific,
-                c.denumire AS clasa,
-                o.denumire AS origine,
+
+                c.denumire AS clasa_animal,
+                o.denumire AS origine_animal,
                 r.denumire AS regim_alimentar,
-                s.denumire AS statut,
+                s.denumire AS statut_conservare,
                 cl.denumire AS clima,
                 mi.denumire AS mod_inmultire,
+
                 a.are_blana,
                 a.poate_fi_dresat,
                 a.este_periculos,
                 a.descriere_ro,
                 a.descriere_en,
-                img.url_imagine
+
+                img.url_imagine,
+
+                (
+                    SELECT LISTAGG(a2.nume_popular, ', ') WITHIN GROUP (ORDER BY a2.nume_popular)
+                    FROM Dusmani_Naturali dn
+                    JOIN Animale a2 ON dn.id_pradator = a2.id_animal
+                    WHERE dn.id_prada = a.id_animal
+                ) AS dusmani_naturali
+
             FROM Animale a
             JOIN Clase_Animale c ON a.id_clasa = c.id_clasa
             JOIN Origini o ON a.id_origine = o.id_origine
@@ -35,39 +46,71 @@ class AnimalModel
             JOIN Statute_Conservare s ON a.id_statut = s.id_statut
             JOIN Clime cl ON a.id_clima = cl.id_clima
             JOIN Moduri_Inmultire mi ON a.id_inmultire = mi.id_inmultire
-            LEFT JOIN Imagini_Animale img 
-                ON a.id_animal = img.id_animal 
+
+            LEFT JOIN Imagini_Animale img
+                ON a.id_animal = img.id_animal
                 AND img.este_imagine_principala = 1
-            ORDER BY a.id_animal
+
+            WHERE 1 = 1
         ";
 
-        $statement = oci_parse($this->conn, $sql);
-        oci_execute($statement);
+        $allowedFilters = [
+            'id_clasa',
+            'id_origine',
+            'id_regim',
+            'id_clima',
+            'are_blana',
+            'poate_fi_dresat',
+            'este_periculos'
+        ];
 
-        $animals = [];
+        foreach ($filters as $column => $value) {
+            if (in_array($column, $allowedFilters)) {
+                $sql .= " AND a.$column = :$column";
+            }
+        }
 
-        while ($row = oci_fetch_assoc($statement)) {
-            $animals[] = [
+        $sql .= " ORDER BY a.id_animal";
+
+        $stmt = oci_parse($this->conn, $sql);
+
+        foreach ($filters as $column => $value) {
+            if (in_array($column, $allowedFilters)) {
+                oci_bind_by_name($stmt, ":$column", $filters[$column]);
+            }
+        }
+
+        oci_execute($stmt);
+
+        $results = [];
+
+        while ($row = oci_fetch_assoc($stmt)) {
+            $results[] = [
                 "id" => $row["ID_ANIMAL"],
                 "nume_popular" => $row["NUME_POPULAR"],
                 "nume_stiintific" => $row["NUME_STIINTIFIC"],
-                "clasa" => $row["CLASA"],
-                "origine" => $row["ORIGINE"],
+
+                "clasa" => $row["CLASA_ANIMAL"],
+                "origine" => $row["ORIGINE_ANIMAL"],
                 "regim_alimentar" => $row["REGIM_ALIMENTAR"],
-                "statut" => $row["STATUT"],
+                "statut" => $row["STATUT_CONSERVARE"],
                 "clima" => $row["CLIMA"],
                 "mod_inmultire" => $row["MOD_INMULTIRE"],
-                "are_blana" => (int)$row["ARE_BLANA"],
-                "poate_fi_dresat" => (int)$row["POATE_FI_DRESAT"],
-                "este_periculos" => (int)$row["ESTE_PERICULOS"],
+
+                "are_blana" => (int) $row["ARE_BLANA"],
+                "poate_fi_dresat" => (int) $row["POATE_FI_DRESAT"],
+                "este_periculos" => (int) $row["ESTE_PERICULOS"],
+
                 "descriere_ro" => $row["DESCRIERE_RO"],
                 "descriere_en" => $row["DESCRIERE_EN"],
-                "url_imagine" => $row["URL_IMAGINE"]
+
+                "url_imagine" => $row["URL_IMAGINE"],
+                "dusmani_naturali" => $row["DUSMANI_NATURALI"]
             ];
         }
 
-        oci_free_statement($statement);
+        oci_free_statement($stmt);
 
-        return $animals;
+        return $results;
     }
 }
